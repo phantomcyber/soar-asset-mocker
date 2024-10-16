8e83caf2a6e54e29c8012af87fa72233ef34754a
Michał Posłuszny <mposluszny@splunk.com>
2024-10-16 19:49:06 +0200

    comment explaining why content after ; is remoed

--- soar_asset_mocker/cli/injector.py
+++ soar_asset_mocker/cli/injector.py
@@ -27,6 +27,7 @@ def export_requirements_to_string():
 
     exporter.export("requirements.txt", Path.cwd(), io)
     requirements = ""
+    # Poetry generate `requirements.txt` file that contains specification of Python version after semicolon, as connectors are bound to specific version it is not relevant here.
     for line in out.fetch().splitlines():
         requirements += line[: line.find(";")] + "\n"
     return requirements
